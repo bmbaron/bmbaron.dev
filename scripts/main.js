9624cdf58e8523a2fda59ec8f38580ffd38adcc6
bmbaron <benjamin.m.baron@gmail.com>
2024-11-20 01:39:05 +0700

    build out about section and refactor css

--- scripts/main.js
+++ scripts/main.js
@@ -11,23 +11,25 @@ const loadContent = (url, elementId) => {
 		.then(data => {
 			document.getElementById(elementId).innerHTML = data;
 			if (elementId === 'footer') {
-				const date = new Date().getFullYear();
+				const date = new Date().getFullYear() + '&nbsp;';
 				document.getElementById('copyright').innerHTML += date;
+				document.getElementById('copyright').innerHTML += 
+					'<a target="_blank" href="https://github.com/bmbaron" title="open my GitHub profile">@bmbaron</a>';
 			}
 		})
 		.catch(error => {
-			console.error("Error loading content:", error);
+			console.error('Error loading content:', error);
 		});
 }
 
 const sections = [
-	{url: 'sections/1-header.html', id: 'header'},
-	{url: 'sections/2-hero.html', id: 'hero'},
-	{url: 'sections/3-about.html', id: 'about'},
-	{url: 'sections/4-services.html', id: 'services'},
-	{url: 'sections/5-projects.html', id: 'projects'},
-	{url: 'sections/6-contact.html', id: 'contact'},
-	{url: 'sections/7-footer.html', id: 'footer'}
+	{url: 'sections/header.html', id: 'header'},
+	{url: 'sections/hero.html', id: 'hero'},
+	{url: 'sections/about.html', id: 'about'},
+	{url: 'sections/services.html', id: 'services'},
+	{url: 'sections/projects.html', id: 'projects'},
+	{url: 'sections/contact.html', id: 'contact'},
+	{url: 'sections/footer.html', id: 'footer'}
 ];
 
 const populateHTML = () => {
@@ -38,6 +40,8 @@ const populateHTML = () => {
 const state = {
 	isHidden: true,
 	hamburgerButton: null,
+	aboutImage: null,
+	aboutImageCaption: null,
 	project1Button: null,
 	project2Button: null,
 	projectItem1: null,
@@ -49,22 +53,26 @@ const state = {
 	hamburgerIcon: null,
 	closeIcon: null,
 	mobileNav: null,
+	mobileLinks: [null],
 	form: null,
 };
 const updateState = () => {
-	state.hamburgerButton = document.getElementById("hamburger");
-	state.project1Button = document.getElementById("project-1");
-	state.project2Button = document.getElementById("project-2");
+	state.hamburgerButton = document.getElementById('hamburger');
+	state.aboutImage = document.getElementById('about-image');
+	state.aboutImageCaption = document.getElementById('about-image-caption');
+	state.project1Button = document.getElementById('project-1');
+	state.project2Button = document.getElementById('project-2');
 	state.projectItem1 = document.getElementById('project-item-1');
 	state.projectItem2 = document.getElementById('project-item-2')
-	state.firstImage1 = document.getElementById("first-image-1");
-	state.secondImage1 = document.getElementById("second-image-1");
-	state.firstImage2 = document.getElementById("first-image-2");
-	state.secondImage2 = document.getElementById("second-image-2");
-	state.hamburgerIcon = document.getElementById("hamburger-icon");
-	state.closeIcon = document.getElementById("close-icon");
-	state.mobileNav = document.getElementById("mobile-nav");
-	state.form = document.getElementById("form");
+	state.firstImage1 = document.getElementById('first-image-1');
+	state.secondImage1 = document.getElementById('second-image-1');
+	state.firstImage2 = document.getElementById('first-image-2');
+	state.secondImage2 = document.getElementById('second-image-2');
+	state.hamburgerIcon = document.getElementById('hamburger-icon');
+	state.closeIcon = document.getElementById('close-icon');
+	state.mobileNav = document.getElementById('mobile-nav');
+	state.mobileLinks = document.getElementsByClassName('mobile-link');
+	state.form = document.getElementById('form');
 };
 const smoothScrollToAnchor = () => {
 	const links = document.querySelectorAll('a[href^="#"]');
@@ -74,12 +82,12 @@ const smoothScrollToAnchor = () => {
 			const href = this.getAttribute('href');
 			const target = document.querySelector(href);
 			if (target) {
-				target.scrollIntoView({behavior: "smooth", block: "start"});
+				target.scrollIntoView({behavior: 'smooth', block: 'start'});
 				link.active = true;
 				state.mobileNav.style.height = '0px';
 				state.mobileNav.style.marginTop = '-100px';
-				state.hamburgerIcon.classList.remove("hidden");
-				state.closeIcon.classList.add("hidden");
+				state.hamburgerIcon.classList.remove('hidden');
+				state.closeIcon.classList.add('hidden');
 				document.body.style.overflowY = 'auto';
 				state.isHidden = !state.isHidden;
 			} else {
@@ -89,14 +97,23 @@ const smoothScrollToAnchor = () => {
 	});
 }
 
-const showMenu = () => {
-	if (state.mobileNav) {
+const showMobileMenu = () => {
+	if (state.mobileNav && state.mobileLinks) {
+		const linksArray = Array.from(state.mobileLinks);
 		if (state.isHidden) {
 			state.mobileNav.style.height = 'calc(100vh - 60px)';
 			state.mobileNav.style.marginTop = '0px';
+			state.mobileNav.style.opacity = '1';
+			linksArray.forEach((link) => {
+				link.style.color = 'var(--primary-color)';
+			})
 		} else {
 			state.mobileNav.style.height = '0px';
 			state.mobileNav.style.marginTop = '-100px';
+			state.mobileNav.style.opacity = '0';
+			linksArray.forEach((link) => {
+				link.style.color = 'var(--off-white)';
+			})
 		}
 	}
 }
@@ -123,33 +140,48 @@ const handleProjectToggle = (buttonClicked) => {
 	state.projectItem2.style.display = 'flex';
 }
 
-window.addEventListener("DOMContentLoaded", async () => {
+const animateAboutImage = (aboutImage, aboutImageCaption) => {
+	if (aboutImage.src.includes('1')) {
+		aboutImage.src = '../images/about-2.webp';
+		aboutImageCaption.innerHTML = `Ahhhh, that's nice. Click to give me a bug to fix`;
+	}
+	else {
+		aboutImage.src = '../images/about-1.webp';
+		aboutImageCaption.innerHTML = 'Alright, what even is this? Click to give me another coffee';
+	}
+}
+window.addEventListener('DOMContentLoaded', async () => {
 	await populateHTML();
 	loadIconScript();
 	updateState();
 	smoothScrollToAnchor();
 	if (state.hamburgerButton) {
-		state.hamburgerButton.addEventListener("click", () => {
-			showMenu();
+		state.hamburgerButton.addEventListener('click', () => {
+			showMobileMenu();
 			if (state.isHidden) {
-				state.hamburgerIcon.classList.add("hidden");
-				state.closeIcon.classList.remove("hidden");
+				state.hamburgerIcon.classList.add('hidden');
+				state.closeIcon.classList.remove('hidden');
 				document.body.style.overflowY = 'hidden';
 			} else {
-				state.hamburgerIcon.classList.remove("hidden");
-				state.closeIcon.classList.add("hidden");
+				state.hamburgerIcon.classList.remove('hidden');
+				state.closeIcon.classList.add('hidden');
 				document.body.style.overflowY = 'auto';
 			}
 			state.isHidden = !state.isHidden;
 		});
 	}
+	if (state.aboutImage && state.aboutImageCaption) {
+		state.aboutImage.addEventListener('click', () => {
+			animateAboutImage(state.aboutImage, state.aboutImageCaption);
+		});
+	}
 	if (state.project1Button && state.project2Button) {
-		state.project1Button.addEventListener("click", () => {
+		state.project1Button.addEventListener('click', () => {
 			state.project2Button.classList.add('button-inverse');
 			state.project1Button.classList.remove('button-inverse');
 			handleProjectToggle(1);
 		});
-		state.project2Button.addEventListener("click", () => {
+		state.project2Button.addEventListener('click', () => {
 			state.project1Button.classList.add('button-inverse');
 			state.project2Button.classList.remove('button-inverse');
 			handleProjectToggle(2);
@@ -183,7 +215,7 @@ if (window.innerWidth <= 1000) {
 }
 const intersectionOptions = {
 	root: null,
-	rootMargin: "0px",
+	rootMargin: '0px',
 	threshold: isMobile ? 0.2 : 0.5,
 };
 
@@ -197,7 +229,7 @@ intersectionProjectsHandler = (entries, observer) => {
 };
 
 let observer = new IntersectionObserver(intersectionProjectsHandler, intersectionOptions);
-const projects = document.getElementById("projects");
+const projects = document.getElementById('projects');
 observer.observe(projects);
 
 
