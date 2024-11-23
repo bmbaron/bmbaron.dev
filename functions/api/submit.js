680e7bff51b5a92b781739b80a43590860f2c089
bmbaron <benjamin.m.baron@gmail.com>
2024-11-23 22:21:13 +0700

    attempt to read env from Cloudflare

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -3,8 +3,10 @@
  */
 export async function onRequestPost(context) {
 	try {
+		console.log(context.env.RECAPTCHA_SECRET_KEY)
 		let input = await context.request.formData();
-		let pretty = JSON.stringify([...input], null, 2);
+		let pretty = JSON.stringify([...input, {"env": context.env.RECAPTCHA_SECRET_KEY}], null, 2);
+
 		return new Response(pretty, {
 			headers: {
 				"Content-Type": "application/json;charset=utf-8",
