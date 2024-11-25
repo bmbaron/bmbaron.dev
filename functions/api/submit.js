9a563bb494d2e01f6e00195f075c52ab63a30b70
bmbaron <benjamin.m.baron@gmail.com>
2024-11-25 17:03:54 +0700

    fixed up recaptcha

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -6,7 +6,6 @@ export async function onRequestPost(context) {
 		const recaptchaSecretKey = context.env.RECAPTCHA_SECRET_KEY;
 		const formData = await context.request.formData();
 		const formObject = Object.fromEntries(formData.entries())
-
 		const token = formObject['g-recaptcha-response'];
 
 		if (!token) {
@@ -14,52 +13,19 @@ export async function onRequestPost(context) {
 		}
 
 		const verificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecretKey}&response=${token}`;
-
 		const verificationResponse = await fetch(verificationUrl, {
 			method: 'POST',
 		});
-
 		const verificationResult = await verificationResponse.json();
 
 		console.log(verificationResult);
-
-
-
-		console.log(JSON.stringify(verificationResponse));
 
 		if (!verificationResult.success) {
 			console.error(JSON.stringify(verificationResult));
 			return new Response(JSON.stringify({ success: false, message: 'Form recaptcha could not be validated' }), { status: 403 });
 
 		}
-
 		return new Response(JSON.stringify({ success: true, message: 'Form recaptcha validated successfully' }), { status: 200 });
-
-
-		// const firstTimeVerifUrl = `https://recaptchaenterprise.googleapis.com/v1/projects/bmbaron-dev-1731665083848/assessments?key=${googleCloudAPIKey}`
-		//
-		// const firstTimeVerifResponse = await fetch(firstTimeVerifUrl, {
-		// 	headers: {
-		// 		"Content-Type": "application/json",
-		// 	},
-		// 	method: "POST",
-		// 	body: JSON.stringify({
-		// 		event: {
-		// 			token: `${token}`,
-		// 			siteKey: "6Ldo9H8qAAAAAOL_iJ8zY8jcJufd3O_sS-LY2AFx"
-		// 		}
-		// 	})
-		// });
-		//
-		// console.log(firstTimeVerifUrl)
-
-
-		// return new Response(JSON.stringify(formObject), {
-		// 	headers: {
-		// 		"Content-Type": "application/json;charset=utf-8",
-		// 	},
-		// });
-
 
 	} catch (error) {
 		return new Response(JSON.stringify({ success: false, error: error.message }), { status: 500 });
